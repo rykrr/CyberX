1b589efa9404f05de6a2e173fd21e7438256b634
Ryan Kerr <ryan@rykr.ca>
2022-02-18 17:54:50 -0500

    Update start.sh

--- iptables/start.sh
+++ iptables/start.sh
@@ -9,6 +9,10 @@ add_chain iptables $addr4
 intercept iptables $addr4 80  8080
 intercept iptables $addr4 443 8443
 
+add_chain ip6tables $addr6
+intercept ip6tables $addr6 80  8080
+intercept ip6tables $addr6 443 8443
+
 if [[ `ip rule list fwmark 1/1 | wc -l` -eq 0 ]]; then
         ip rule add fwmark 1/1 table $rt_table
         ip route add local 0.0.0.0/0 dev lo table $rt_table
